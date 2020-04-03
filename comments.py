1b8910a60aecf0276e57ddd0ee4f9a656f1e4744
Amy Zhou <amy_zhou@college.harvard.edu>
2020-04-03 19:39:09 +1000

    updated scratch comments to also include usernames of people who commented

--- comments.py
+++ comments.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import argparse
 import json
 import os
@@ -11,11 +12,8 @@ def get_arguments():
 
     # Arguments related to input
     inputs = parser.add_mutually_exclusive_group(required=True)
-    inputs.add_argument("-s", dest="studio", nargs="*", help="Studio ID. Will scrape all projects from the studio with the given ID.")
+    #inputs.add_argument("-s", dest="studio", nargs="*", help="Studio ID. Will scrape all projects from the studio with the given ID.")
     inputs.add_argument("-p", dest="project", nargs="*", help="Project ID. Will scrape one project for each ID provided.")
-    inputs.add_argument("-f", dest="studio_list", nargs="*", help="File name for a line-separated list of studio URLs (or IDs). Will scrape all projects in all studios.")
-    inputs.add_argument("-g", dest="project_list", nargs="*", help="File name for a line-separated list of project URLs (or IDs). Will scrape all projects.")
-
     # Arguments related to output
     parser.add_argument("-d", dest="output_directory", help="Output directory. Will save output to this directory, and create the directory if doesn’t exist.")
     parser.add_argument("-n", dest="output_name", help="Name of the output JSON file, if only a single output file is desired. Otherwise, will save projects to individual JSON files.")
@@ -45,17 +43,20 @@ def get_project_ids(arguments):
 def download_page(id_set, output_directory=os.getcwd(), file_name=None):
     page = 1
     master_comments = []
+    master_users = []
     master_count = 0
+
     while True:
         try: 
             comment_list, comment_count = download_comments(id_set, page)
+            user_list = download_users(id_set, page)
             master_comments.extend(comment_list)
+            master_users.extend(user_list)
             master_count += comment_count
             page += 1
         except:
             break
-    print(master_comments, master_count)
-    return (master_comments, master_count)
+    return (master_comments, master_users, master_count)
 
 # Downloads comments for the given project ID and page number
 def download_comments(id_set, page, output_directory=os.getcwd(), file_name=None):
@@ -68,6 +69,7 @@ def download_comments(id_set, page, output_directory=os.getcwd(), file_name=None
         raise RuntimeError("GET {0} failed with status code {1}".format(r.status_code, url))
     # Use Beautiful Soup to scrape the webpage for the comments that we want
     soup = BeautifulSoup(r.content, 'html.parser')
+    #print(soup)
     all_comments = soup.select(".content")
     # Go through each comment and clean, adding comment to comments list
     for comment in all_comments:
@@ -76,6 +78,23 @@ def download_comments(id_set, page, output_directory=os.getcwd(), file_name=None
             comments.append(clean_comment)
             comment_count += 1
     return (comments, comment_count)
+
+# Returns a list of the usernames that commented on the project
+def download_users(id_set, page, output_directory=os.getcwd(), file_name=None):
+    users = []
+    for id in id_set:
+        url = COMMENTS_URL.format(id, page)
+    r = requests.get(url)
+    if r.status_code != 200:
+        raise RuntimeError("GET {0} failed with status code {1}".format(r.status_code, url))
+    # Use Beautiful Soup to scrape the webpage for the comments that we want
+    soup = BeautifulSoup(r.content, 'html.parser')
+    # Go through each user, adding user to users list
+    all_users = soup.select(".name")
+    for user in all_users:
+        clean_user = user.get_text().strip()
+        users.append(clean_user)
+    return users
 
 def main():
     arguments = get_arguments()
